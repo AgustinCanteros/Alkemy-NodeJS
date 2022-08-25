40d2ca936ca5d7a055066a9694da553ec48398a3
Agustin Canteros <canterosagustindev@gmail.com>
2022-08-25 12:25:30 -0300

    modelo de usuarios

---
+++ src/models/Usuarios.js
@@ -0,0 +1,26 @@
+import { DataTypes } from "../../node_modules/sequelize/index";
+import { sequelize } from "../database/db.js";
+
+export const Usuario = sequelize.define("usuario", {
+  nombre: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+  apellido: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+  email: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+  password: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+  id: {
+    type: DataTypes.UUIDV4,
+    default: sequelize.UUIDV4,
+    primaryKey: true,
+  },
+});
